33d09ac11a91633f93faf4c279bd5559399bd549
Antoine Laurent <alaurent@agencemobilitedurable.ca>
2023-11-21 11:05:26 -0500

    chore: update for postgresql config

--- utils.py
+++ utils.py
@@ -1,18 +1,20 @@
+import urllib.parse
 import pandas as pd
 import geopandas as gpd
 from geopandas import GeoDataFrame
 from shapely.wkt import loads
 from sqlalchemy import create_engine, text, types
 
-def getEngine(server, database, driver, Trusted_Connection='yes', autocommit=True, fast_executemany=True):
+def getEngine(server, database, driver, Trusted_Connection='yes', autocommit=True,
+              fast_executemany=True, user='', pwd=''):
     """ Create a connection to a sql server via sqlalchemy
     Arguments:
     server -- The server name (str). e.g.: 'SQL2012PROD03'
     database -- The specific database within the server (str). e.g.: 'LowFlows'
     driver -- The driver to use for the connection (str). e.g.: SQL Server
-    trusted_conn -- Is the connection to be trusted. Values are 'yes' or 'No' (str). 
+    trusted_conn -- Is the connection to be trusted. Values are 'yes' or 'No' (str).
     """
-    
+
     if driver == 'SQL Server':
         engine = create_engine(
             f"mssql+pyodbc://{server}/{database}"
@@ -21,7 +23,13 @@ def getEngine(server, database, driver, Trusted_Connection='yes', autocommit=Tru
             f"&autocommit={autocommit}",
             fast_executemany=fast_executemany
         )
-    else: 
+    elif driver == 'postgresql':
+        user = urllib.parse.quote_plus(user)
+        pwd  = urllib.parse.quote_plus(pwd)
+        engine = create_engine(
+            f"postgresql+psycopg2://{user}:{pwd}@{server}/{database}"
+        )
+    else:
         raise NotImplementedError('No other connections supported')
     return engine
 
@@ -59,7 +67,7 @@ def checkTableExists(dbcon, tablename):
 def create_table(dbcon, tablename, tablesql):
     #dbcur = dbcon.cursor()
     dbcur = dbcon.execute("""
-    CREATE TABLE [dbo].[{0}](
+    CREATE TABLE analyses.{0}(
 		{1}
 	)
     """.format(
@@ -71,16 +79,17 @@ def create_table(dbcon, tablename, tablesql):
 def truncate_table(dbcon, tablename):
     # dbcur = dbcon.cursor()
     dbcur = dbcon.execute(f"""
-        TRUNCATE TABLE [dbo].[{tablename}]
+        TRUNCATE TABLE analyses.{tablename}
     """)
     dbcur.close()
 
-def get_last_data_inserted(dbcon, tablename, techno='Genetec', timestamp_col='InstantDeLecture'):
+def get_last_data_inserted(dbcon, tablename, techno='Genetec', timestamp_col='horodatage_lecture'):
     dbcur = dbcon.execute(f"""
-        SELECT TOP (1) {timestamp_col} 
-        FROM [dbo].[{tablename}]
+        SELECT {timestamp_col} at time zone 'America/Montreal'
+        FROM analyses.{tablename}
         WHERE Techno = '{techno}'
         ORDER BY {timestamp_col} DESC
+        LIMIT 1
     """)
     date = pd.to_datetime(dbcur.first()[0])
     dbcur.close()
